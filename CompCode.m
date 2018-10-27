d2716c7116c0ad829432a3c5d2889212329bcf86
Lingfei Song <slf1234@126.com>
2018-10-27 10:13:40 +0800

    Update CompCode.m

--- CompCode.m
+++ CompCode.m
@@ -3,4 +3,4 @@ function [score] = CompCode(palm1, palm2)
 D1 = compcode_coding(palm1);
 D2 = compcode_coding(palm2);
 
-score = compcode_matching_no_align(D1, D2);
+score = compcode_matching(D1, D2);
